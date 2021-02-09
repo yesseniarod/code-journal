a59218a94a3d1e261944eca839481bfb4d1efc12
Yessenia Rodriguez <yessi.rod30@gmail.com>
2021-02-09 15:17:03 -0800

    reset image preview src attribute

--- js/main.js
+++ js/main.js
@@ -3,9 +3,15 @@
 var $inputUrl = document.querySelector('input');
 var $image = document.querySelector('img');
 var $form = document.querySelector('form');
+// console.log($inputUrl.value.length);
 
 function addImage(event) {
-  $image.setAttribute('src', $form.elements.image.value);
+
+  if ($inputUrl.value.length !== 0) {
+    $image.setAttribute('src', $form.elements.image.value);
+  } else {
+    $image.setAttribute('src', './images/placeholder-image-square.jpg');
+  }
 }
 
 $inputUrl.addEventListener('input', addImage);
