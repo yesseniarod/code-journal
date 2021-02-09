5da331a1237f000352d1fd5209b629d0bc7751e7
Yessenia Rodriguez <yessi.rod30@gmail.com>
2021-02-08 21:36:07 -0800

    image input event listener

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 /* global data */
 /* exported data */
+var $inputUrl = document.querySelector('input');
+var $image = document.querySelector('img');
+// var $value = $inputUrl.value;
+
+function addImage(event) {
+  // var updateImage =
+  $image.setAttribute('src', 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12213218/German-Shepherd-on-White-00.jpg');
+}
+
+$inputUrl.addEventListener('input', addImage);
